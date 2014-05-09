6bfb07359d14340bd4b5d9ef923366ecaa87c1ea
kd8bny <kd8bny@gmail.com>
2014-05-09 15:07:32 -0400

    Verboseish

--- makeQWT.py
+++ makeQWT.py
@@ -1,6 +1,6 @@
 #Daryl W. Bennett --kd8bny@gmail.com
 #Allow the edit of QT Designer files for QWT support
-#Custom UI input
+#TODO: Custom UI input
 
 #V1 R0
 import string, os
@@ -8,11 +8,13 @@ import string, os
 def makeQWT():
 	edit = '#Edited By: #Daryl W. Bennett --kd8bny@gmail.com \n#QWT5 support added using makeQWT V1 R0 \n\n'
 	QWT = 'from PyQt4.Qwt5 import * \n'
-	temp = open('main.py', 'r+')
+	print "\nmakeQWT V1 loading QT file\n"
+	temp = open('../main.py', 'r+')
 	lines = temp.readlines()
 	lines.insert(9,edit)
 	lines.insert(11,QWT)
 	temp.seek(0)
+	print "\nWriting QWT5 import line\n"
 	temp.writelines(lines)
 	temp.close()
 
